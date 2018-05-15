48d4df418c3e0eb5f3540389fec9f825dd77043b
John Matlock <john.matlock@rackspace.com>
2018-05-14 19:49:44 -0700

    Dependency loading changes, manifest file changes, added documentation, layout changes
    Broke out libraries sections and scripts section from manifest
    Added shortname to manifest file
    Added documentation to module main function
    Modified method for loading dependencies.  Note that I cannot functionalize loading the library files as they do not get left in the root scope outside of the function.
    Changed layout of the module script to better conform to standards

--- src/EnvironmentDiscoveryUtility.psm1
+++ src/EnvironmentDiscoveryUtility.psm1
@@ -1,6 +1,6 @@
 ﻿#region Functions
 
-function Get-LibraryManifest
+function GetLibraryManifest
 {
     $libraryManifestPath = "$(split-path $SCRIPT:MyInvocation.MyCommand.Path -parent)\library-manifest.json"
 
@@ -26,26 +26,87 @@ function Get-LibraryManifest
     $libraryManifest
 }
 
+function ConverRelativePathToLiteralPath
+{
+    Param (
+        [String]
+        $RelativePath
+    )
+
+    $literalPath = "$(split-path $SCRIPT:MyInvocation.MyCommand.Path -parent)$( $RelativePath )"
+    $literalPath
+}
+
 function Start-EnvironmentDiscovery
 {
-    $forestDetails = Get-ADForestDetails
-    if ($forestDetails)
+        <#
+    .SYNOPSIS
+        This cmdlet will start a run of the Environment Discovery Utility.  
+
+    .DESCRIPTION
+        This cmdlet will start a run of the Environment Discovery Utility.  This utility gathers important information regarding Microsoft products for the purpose of evaluating customer environments to aid in the scoping of projects.
+
+    .PARAMETER Modules
+        An array of strings indicating which modules the Environment Discovery Utility should run.  This defaults to 'All'
+
+    .OUTPUTS
+        A JSON representation of the discovered environment.
+
+    .EXAMPLE
+        Start-EnvironmentDiscovery -Modules All
+
+    .EXAMPLE
+        Start-EnvironmentDiscovery -Modules Exchange,AD
+
+    #>
+
+    Param (
+        # An array of strings indicating which modules the Environment Discovery Utility should run.  Possible values: AD, Exchange, All.  This defaults to 'All'
+        [ValidateSet("ad","exchange","all")] 
+        [String] 
+        $Modules 
+    )
+    
+    BEGIN
     {
-        SerializeTo-Json $forestDetails
+        $manifest = GetLibraryManifest
+    
+        foreach ($script in $manifest.scripts)
+        {
+            $path = ConverRelativePathToLiteralPath -RelativePath $script.path
+            . "$($path)" > $null
+        }
+
+        if($Modules -like 'All')
+        {
+            $Modules = $manifest.libaries.ShortName
+        }
+        
+        foreach($module in $modules)
+        {
+            if($Modules -contains $module.ToLower())
+            {
+                foreach ($library in $manifest.libraries)
+                {
+                    $libraryPath = ConverRelativePathToLiteralPath -RelativePath $library.path
+                    $scriptFiles = Get-ChildItem -Path $libraryPath *.ps1
+                    
+                    foreach ($path in $scriptFiles.FullName)
+                    {
+                        . "$($path)" > $null
+                    }
+                }
+            }
+        }
+    }
+    PROCESS
+    {
+        $forestDetails = Get-ADForestDetails | SerializeTo-Json
+    }
+    END
+    {
+        $forestDetails
     }
 }
 
 #endregion
-
-#region Initialize Module
-
-$manifest = Get-LibraryManifest
-
-foreach ($library in $manifest.libraries)
-{
-    $libraryPath = "$(split-path $SCRIPT:MyInvocation.MyCommand.Path -parent)$($library.path)"
-    . "$($libraryPath)" > $null
-}
-
-#endregion
-
