be11a6bc959b0adeaf4d98ae527076c3f0afe527
Chad Matlock <chad.matlock@RACKSPACE.COM>
2018-05-29 11:31:15 -0500

    Error Logic
    Added logic to ensure PF are there before running

--- src/exchange/Get-ExchangePublicFolderStatistics.ps1
+++ src/exchange/Get-ExchangePublicFolderStatistics.ps1
@@ -8,29 +8,32 @@ function Get-ExchangePublicFolderStatistics
 
     if ($ExchangeShellConnected)
     {
-        $publicFolderStatistics = Get-PublicFolderStatistics -resultSize Unlimited | Select-Object name,folderPath,itemCount,totalItemSize
-        $discoveredPublicFolderStatistics = $null
-        foreach ($publicFolderStatistic in $publicFolderStatistics)
+        if (Get-PublicFolder -ErrorAction SilentlyContinue)
         {
-            
-            $publicFolderStats = $null
-            $publicFolderStats = "" | Select-Object name,folderPath,itemCount,totalItemSize    
-            $publicFolderStats.name = $publicFolderStatistic.name        
-                        
-            if($publicFolderStatistic.folderPath -is [system.array])
+            $publicFolderStatistics = Get-PublicFolderStatistics -resultSize Unlimited | Select-Object name,folderPath,itemCount,totalItemSize
+            $discoveredPublicFolderStatistics = $null
+            foreach ($publicFolderStatistic in $publicFolderStatistics)
             {
-                $publicFolderStats.folderPath = "\" + ($publicFolderStatistic.folderPath) -join '\'
-            }
-            
-            else 
-            {
-                $publicFolderStats.folderPath = $publicFolderStatistic.folderPath
-            }
+                
+                $publicFolderStats = $null
+                $publicFolderStats = "" | Select-Object name,folderPath,itemCount,totalItemSize    
+                $publicFolderStats.name = $publicFolderStatistic.name        
+                            
+                if($publicFolderStatistic.folderPath -is [system.array])
+                {
+                    $publicFolderStats.folderPath = "\" + ($publicFolderStatistic.folderPath) -join '\'
+                }
+                
+                else 
+                {
+                    $publicFolderStats.folderPath = $publicFolderStatistic.folderPath
+                }
 
-            $publicFolderStats.itemCount = $publicFolderStatistic.itemCount
-            $publicFolderStats.totalItemSize = $publicFolderStatistic.totalItemSize
-            $discoveredPublicFolderStatistics += $PublicFolderStats
+                $publicFolderStats.itemCount = $publicFolderStatistic.itemCount
+                $publicFolderStats.totalItemSize = $publicFolderStatistic.totalItemSize
+                $discoveredPublicFolderStatistics += $PublicFolderStats
+            }
+            $discoveredPublicFolderStatistics
         }
-        $discoveredPublicFolderStatistics
     }
 }
