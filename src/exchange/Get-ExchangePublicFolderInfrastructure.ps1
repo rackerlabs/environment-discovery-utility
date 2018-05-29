b7d487b79d5c79ee1b80fc518c03649405190fca
Chad Matlock <chad.matlock@RACKSPACE.COM>
2018-05-29 12:26:32 -0500

    reverted Changes

--- src/exchange/Get-ExchangePublicFolderInfrastructure.ps1
+++ src/exchange/Get-ExchangePublicFolderInfrastructure.ps1
@@ -7,20 +7,25 @@ function Get-ExchangePublicFolderInfrastructure
     )
 
     $discoveredPublicFolderMailboxes = @()
-    $ldapFilter = "(&(msExchRecipientTypeDetails=68719476736)(objectClass=msExchPublicMDB))"
+    $ldapFilter = "(msExchRecipientTypeDetails=68719476736)"
     $context = "LDAP://$($DomainDN)"
     $searchRoot = "$($DomainDN)"
     [array] $properties = "name","homeMDB"
     $pfMailboxes = Search-Directory -context $context -Filter $ldapFilter -Properties $properties -SearchRoot $searchRoot
 
-    foreach ($pfMailbox in $pfMailboxes)
+    if ($pfMailboxes)
     {
-        $publicFolderMailboxes = $null
-        $publicFolderMailboxes = "" | Select-Object objectGuid, pfMBXName, parentDatabase
-        $publicFolderMailboxes.objectGuid = [GUID]$pfMailbox.objectGuid
-        $publicFolderMailboxes.pfMBXName = $pfMailbox.name
-        $publicFolderMailboxes.parentDatabase = $pfMailbox.homeMDB
-        $discoveredPublicFolderMailboxes += $publicFolderMailboxes
+        foreach ($pfMailbox in $pfMailboxes)
+        {
+            $publicFolderMailboxes = $null
+            $publicFolderMailboxes = "" | Select-Object objectGuid, pfMBXName, parentDatabase
+            $publicFolderMailboxes.objectGuid = [GUID]$pfMailbox.objectGuid
+            $publicFolderMailboxes.pfMBXName = $pfMailbox.name
+            $publicFolderMailboxes.parentDatabase = $pfMailbox.homeMDB
+
+            $discoveredPublicFolderMailboxes += $publicFolderMailboxes
+        }
+
         $discoveredPublicFolderMailboxes
     }
 }
