483115b1732fc7c0d5fb2cbb0a62932ac42d8175
James Blake <james.blake@rackspace.com>
2018-05-29 13:59:11 -0500

    Zipping
    Add zip script and library for testing and deployment.  Add basic jenkinsfile which performs basic build steps, zips files, logs output slack.

---
+++ build/Build-ZipFile.ps1
@@ -0,0 +1,58 @@
+<#
+
+.SYNOPSIS
+	Zip all EDU files.
+
+.DESCRIPTION
+	Zip EDU scripts and libaries into one file for testing and distribution.
+
+.PARAMETER BuildNumber
+	EDU build number.
+
+.EXAMPLE
+	Build-ZipFile -BuildNumber 25
+
+#>
+
+[CmdletBinding()]
+param (
+	[Parameter(Mandatory=$true)]
+	[string]
+	$BuildNumber
+)
+
+function Add-ZipFiles
+{	
+	foreach ($file in @($input))
+	{
+		$file.VersionInfo.FileName
+		$relativePath = $file.VersionInfo.FileName -replace [Regex]::Escape($rootDir), ""
+		$relativePath = $relativePath -replace [Regex]::Escape($file.Name), ""
+		$relativePath
+		$zipFile.AddFile($file.VersionInfo.FileName, $relativePath) | Out-Null
+	}
+}
+
+function Zip-Scripts()
+{
+	gci "$rootDir\*.*" -Recurse | Add-ZipFiles
+}
+
+Set-Location -Path "$PSScriptRoot\..\src"
+$rootDir = (Get-Location).Path
+Set-Location -Path "$PSScriptRoot"
+
+Add-Type -Path "$PSScriptRoot\Ionic.Zip.dll"
+$zipFile = New-Object Ionic.Zip.ZipFile
+$zipPath = "$PSScriptRoot\edu.v$BuildNumber.zip"
+
+Zip-Scripts
+
+if (Test-Path $zipPath)
+{
+	Remove-Item -Force -Confirm:$false $zipPath
+}
+
+$zipFile.UseZip64WhenSaving = "AsNecessary"
+$zipFile.Save($zipPath)
+$zipFile.Dispose()
