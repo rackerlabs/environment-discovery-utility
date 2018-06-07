b96d615460f5513ae2bdff8ddf23de57dfd3e75e
Chad Matlock <chad.matlock@RACKSPACE.COM>
2018-06-07 10:38:24 -0500

    Try/catch wrapper
    Taking James's feedback I added a try catch to ensure the global setting is always set back to continue

--- src/exchange/Get-ExchangeTransportRules.ps1
+++ src/exchange/Get-ExchangeTransportRules.ps1
@@ -8,50 +8,57 @@ function Get-ExchangeTransportRules
         [bool]
         $ExchangeShellConnected
     )
+    try 
+    {    
+        $ErrorActionPreference = "silentlycontinue"
+        $discoveredTransportRules = @()
+        $searchRoot = "CN=Configuration,$($DomainDN)"
+        $ldapFilter = "(objectClass=msExchTransportRule)"
+        $context = "LDAP://CN=Configuration,$($DomainDN)"
+        [array] $properties = "objectGUID", "distinguishedName"
+        $transportRules = Search-Directory -context $context -Filter $ldapFilter -Properties $properties -SearchRoot $searchRoot
 
-    $ErrorActionPreference = "silentlycontinue"
-    $discoveredTransportRules = @()
-    $searchRoot = "CN=Configuration,$($DomainDN)"
-    $ldapFilter = "(objectClass=msExchTransportRule)"
-    $context = "LDAP://CN=Configuration,$($DomainDN)"
-    [array] $properties = "objectGUID", "distinguishedName"
-    $transportRules = Search-Directory -context $context -Filter $ldapFilter -Properties $properties -SearchRoot $searchRoot
-
-    foreach ($transportRule in $transportRules)
-    {
-        $transportRuleSettings = $null
-        $transportRuleSettings = "" | Select-Object ObjectGUID, Type, Condition, Exemption, Action
-        $transportRuleSettings.ObjectGUID = [GUID] $( $transportRule.objectGUID | Select-Object -First 1 )
-        
-        if (($transportRule.distinguishedName) -like "*TransportVersioned*")
+        foreach ($transportRule in $transportRules)
         {
-            $transportRuleSettings.Type = "TransportRule"
+            $transportRuleSettings = $null
+            $transportRuleSettings = "" | Select-Object ObjectGUID, Type, Condition, Exemption, Action
+            $transportRuleSettings.ObjectGUID = [GUID] $( $transportRule.objectGUID | Select-Object -First 1 )
             
-            if ($ExchangeShellConnected)
+            if (($transportRule.distinguishedName) -like "*TransportVersioned*")
             {
-                $objectGuid = [string]($transportRuleSettings).ObjectGUID
-                $exchangeTransportRule = Get-TransportRule -Identity $objectGuid
-                $transportRuleSettings.Condition = ( $exchangeTransportRule | Select-Object -ExpandProperty Conditions ).name
-                $transportRuleSettings.Exemption = ( $exchangeTransportRule | Select-Object -ExpandProperty Exemptions ).name
-                $transportRuleSettings.Action = ( $exchangeTransportRule | Select-Object -ExpandProperty Actions ).name
+                $transportRuleSettings.Type = "TransportRule"
+                
+                if ($ExchangeShellConnected)
+                {
+                    $objectGuid = [string]($transportRuleSettings).ObjectGUID
+                    $exchangeTransportRule = Get-TransportRule -Identity $objectGuid
+                    $transportRuleSettings.Condition = ( $exchangeTransportRule | Select-Object -ExpandProperty Conditions ).name
+                    $transportRuleSettings.Exemption = ( $exchangeTransportRule | Select-Object -ExpandProperty Exemptions ).name
+                    $transportRuleSettings.Action = ( $exchangeTransportRule | Select-Object -ExpandProperty Actions ).name
+                }
             }
-        }
-        elseif (($transportRule.distinguishedName) -like "*JournalingVersioned*")
-        {
-            $transportRuleSettings.Type = "JournalingRule"
-        }
-        elseif (($transportRule.distinguishedName) -like "*ClassificationDefinitions*")
-        {
-            continue
-        }
-        else
-        {
-            $transportRuleSettings.Type = "Other"
+            elseif (($transportRule.distinguishedName) -like "*JournalingVersioned*")
+            {
+                $transportRuleSettings.Type = "JournalingRule"
+            }
+            elseif (($transportRule.distinguishedName) -like "*ClassificationDefinitions*")
+            {
+                continue
+            }
+            else
+            {
+                $transportRuleSettings.Type = "Other"
+            }
+
+            $discoveredTransportRules += $transportRuleSettings
         }
 
-        $discoveredTransportRules += $transportRuleSettings
+        $discoveredTransportRules
+        $ErrorActionPreference = "continue"
+        
     }
-
-    $discoveredTransportRules
-    $ErrorActionPreference = "continue"
+    catch 
+    {
+        $ErrorActionPreference = "continue"
+    }
 }
