98d88b1ffd9d9980abc7fee1969c553859b5f45a
Chad Matlock <chad.matlock@RACKSPACE.COM>
2018-06-07 14:04:41 -0500

    Main Function Load

---
+++ src/exchange/Get-ExchangeFederationTrust.ps1
@@ -0,0 +1,26 @@
+function Get-ExchangeFederationTrust
+{
+    [CmdletBinding()]
+    param (
+        [string]
+        $DomainDN
+    )
+
+    $discoveredFederationTrusts = @()
+    $ldapFilter = "(objectClass=msExchFedTrust)"
+    $context = "LDAP://CN=Configuration,$($DomainDN)"
+    $searchRoot = "$DomainDN"
+    [array]$properties = "objectGUID"
+    $exchangeFederationTrusts = Search-Directory -Context $context -Filter $ldapFilter -Properties $properties -SearchRoot $searchRoot
+
+    foreach ($exchangefederationTrust in $exchangeFederationTrusts)
+    {
+        $federationTrust = $null
+        $federationTrust = "" | Select-Object objectGUID
+        $federationTrust.ObjectGUID = [GUID] $( $exchangefederationTrust.objectguid | Select-Object -First 1 )
+
+        $discoveredFederationTrusts += $federationTrust
+    }
+
+    $discoveredFederationTrusts
+}
