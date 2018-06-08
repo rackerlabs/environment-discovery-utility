f056a85a29ce062a29b4cf61d7df1b5c8decb665
Chad Matlock <chad.matlock@RACKSPACE.COM>
2018-06-08 08:47:40 -0500

    Spacing Corrections
    Took out spacing to meet standards

--- src/exchange/Get-ExchangeFederation.ps1
+++ src/exchange/Get-ExchangeFederation.ps1
@@ -8,7 +8,7 @@ function Get-ExchangeFederation
 
     $discoveredFederationSettings = @()
     $ldapFilter = "(objectClass=msexchfedsharingrelationship)"
-    $context = "LDAP://CN=Configuration,$($domainDN)"
+    $context = "LDAP://CN=Configuration, $($domainDN)"
     $searchRoot = "$DomainDN"
     [array]$properties = "objectGUID", "msExchFedEnabledActions", "msEXchFedIsEnabled"
     $exchangeFederationPolicys = Search-Directory -Context $context -Filter $ldapFilter -Properties $properties -SearchRoot $searchRoot
@@ -17,7 +17,7 @@ function Get-ExchangeFederation
     {
         $federationPolicy = $null
         $federationPolicy = "" | Select-Object ObjectGUID, FederationEnabled, FederationActions
-        $federationPolicy.ObjectGUID = [GUID] $( $exchangeGederationPolicy.objectGUID | Select-Object -First 1 )
+        $federationPolicy.ObjectGUID = [GUID]$($exchangeGederationPolicy.objectGUID | Select-Object -First 1)
         $federationPolicy.FederationEnabled = $exchangeGederationPolicy.msEXchFedIsEnabled
         $federationPolicy.FederationActions = $exchangeGederationPolicy.msExchFedEnabledActions
 
