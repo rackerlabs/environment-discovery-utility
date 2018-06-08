9174d5116296adc581f0791ea44e124ba035c0f7
Chad Matlock <chad.matlock@RACKSPACE.COM>
2018-06-08 08:39:32 -0500

    Made suggested changes
    Followed suggestions from James and made the same suggestions

--- src/exchange/Get-ExchangeTransportRules.ps1
+++ src/exchange/Get-ExchangeTransportRules.ps1
@@ -8,13 +8,14 @@ function Get-ExchangeTransportRules
         [bool]
         $ExchangeShellConnected
     )
+    
     try 
     {    
         $ErrorActionPreference = "silentlycontinue"
         $discoveredTransportRules = @()
-        $searchRoot = "CN=Configuration,$($DomainDN)"
+        $searchRoot = "CN=Configuration, $($DomainDN)"
         $ldapFilter = "(objectClass=msExchTransportRule)"
-        $context = "LDAP://CN=Configuration,$($DomainDN)"
+        $context = "LDAP://CN=Configuration, $($DomainDN)"
         [array] $properties = "objectGUID", "distinguishedName"
         $transportRules = Search-Directory -context $context -Filter $ldapFilter -Properties $properties -SearchRoot $searchRoot
 
@@ -22,7 +23,7 @@ function Get-ExchangeTransportRules
         {
             $transportRuleSettings = $null
             $transportRuleSettings = "" | Select-Object ObjectGUID, Type, Condition, Exemption, Action
-            $transportRuleSettings.ObjectGUID = [GUID] $( $transportRule.objectGUID | Select-Object -First 1 )
+            $transportRuleSettings.ObjectGUID = [GUID]$($transportRule.objectGUID | Select-Object -First 1)
             
             if (($transportRule.distinguishedName) -like "*TransportVersioned*")
             {
@@ -32,9 +33,9 @@ function Get-ExchangeTransportRules
                 {
                     $objectGuid = [string]($transportRuleSettings).ObjectGUID
                     $exchangeTransportRule = Get-TransportRule -Identity $objectGuid
-                    $transportRuleSettings.Condition = ( $exchangeTransportRule | Select-Object -ExpandProperty Conditions ).name
-                    $transportRuleSettings.Exemption = ( $exchangeTransportRule | Select-Object -ExpandProperty Exemptions ).name
-                    $transportRuleSettings.Action = ( $exchangeTransportRule | Select-Object -ExpandProperty Actions ).name
+                    $transportRuleSettings.Condition = ($exchangeTransportRule | Select-Object -ExpandProperty Conditions).name
+                    $transportRuleSettings.Exemption = ($exchangeTransportRule | Select-Object -ExpandProperty Exemptions).name
+                    $transportRuleSettings.Action = ($exchangeTransportRule | Select-Object -ExpandProperty Actions).name
                 }
             }
             elseif (($transportRule.distinguishedName) -like "*JournalingVersioned*")
@@ -52,13 +53,12 @@ function Get-ExchangeTransportRules
 
             $discoveredTransportRules += $transportRuleSettings
         }
-
-        $discoveredTransportRules
-        $ErrorActionPreference = "continue"
-        
     }
-    catch 
+    
+    finally 
     {
         $ErrorActionPreference = "continue"
     }
+
+    $discoveredTransportRules
 }
