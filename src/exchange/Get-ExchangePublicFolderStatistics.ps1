677724440932fd14abd314b2b396d05435408f8b
Chad Matlock <chad.matlock@RACKSPACE.COM>
2018-06-26 13:41:12 -0500

    Addessed code bugs
    Addressing Bug found in issue #153
    
    closes #153

--- src/exchange/Get-ExchangePublicFolderStatistics.ps1
+++ src/exchange/Get-ExchangePublicFolderStatistics.ps1
@@ -12,27 +12,19 @@ function Get-ExchangePublicFolderStatistics
     $activity = "Public Folder Statistics"
     $discoveredPublicFolderStatistics = @()
 
-    if ($PublicFolders.Mailboxes -or $PublicFolders.Databases)
+    if ($ExchangeShellConnected)
     {
-        if ($ExchangeShellConnected)
+        if ($PublicFolders.Mailboxes)
         {
-            Write-Log -Level "VERBOSE" -Activity $activity -Message "Gathering Public Folder statistics. This may take some time without feedback." -WriteProgress
-            $publicFolderStatistics = Get-PublicFolderStatistics -ResultSize Unlimited
+            Write-Log -Level "VERBOSE" -Activity $activity -Message "Gathering modern public folder statistics. This may take some time without feedback." -WriteProgress
+            $publicFolderStatistics = Get-PublicFolderStatistics
 
             foreach ($publicFolderStatistic in $publicFolderStatistics)
             {
                 $publicFolderStats = $null
                 $publicFolderStats = "" | Select-Object Identity, ItemCount, TotalItemSizeKB
                 $publicFolderStats.ItemCount = $publicFolderStatistic.itemCount
-
-                if ((Get-ExchangeServer $env:ComputerName | Select-Object AdminDisplayVersion) -like "*15.*")
-                {
-                    $publicFolderStats.Identity = $publicFolderStatistic.identity.objectGUID
-                }
-                else
-                {
-                    $publicFolderStats.Identity = $publicFolderStatistic.entryID
-                }
+                $publicFolderStats.Identity = $publicFolderStatistic.identity.objectGUID
 
                 if ($PSVersionTable.PSVersion.Major -ge 3)
                 {
@@ -46,6 +38,46 @@ function Get-ExchangePublicFolderStatistics
                 $discoveredPublicFolderStatistics += $publicFolderStats
             }
         }
+        
+        elseif ($PublicFolders.Databases)
+        {
+            Write-Log -Level "VERBOSE" -Activity $activity -Message "Gathering legacy public folder statistics. This may take some time without feedback." -WriteProgress
+            $publicFolderStatistics = @()
+            foreach ($database in ($PublicFolders.Databases))
+            {
+                [string]$server = $database.ParentServer
+                $version = Get-ExchangeServer $server | Select-Object AdminDisplayVersion
+                if ($version -like "*14.*")
+                {
+                    $publicFolderStatistics += Get-PublicFolderStatistics -Server $server -ResultSize Unlimited
+                }
+                else 
+                {
+                    $publicFolderStatistics += Get-PublicFolderStatistics -Server $server    
+                }
+            }
+
+            $publicFolderStatistics = $publicFolderStatistics | Sort-Object entryID -unique
+            foreach ($publicFolderStatistic in $publicFolderStatistics)
+            {
+                $publicFolderStats = $null
+                $publicFolderStats = "" | Select-Object Identity, ItemCount, TotalItemSizeKB
+                $publicFolderStats.ItemCount = $publicFolderStatistic.itemCount
+                $publicFolderStats.Identity = $publicFolderStatistic.entryID
+
+                if ($PSVersionTable.PSVersion.Major -ge 3)
+                {
+                    $publicFolderStats.TotalItemSizeKB = $publicFolderStatistic.totalItemSize.ToKB()
+                }
+                else
+                {
+                    $publicFolderStats.TotalItemSizeKB = $publicFolderStatistic.totalItemSize.value.ToKB()
+                }
+
+                $discoveredPublicFolderStatistics += $publicFolderStats
+            }
+        }
+        
         else
         {
             Write-Log -Level "WARNING" -Activity $activity -Message "Skipping Exchange Public Folder statistics. No connection to Exchange."
