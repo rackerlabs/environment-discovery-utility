c2ece77f3fca6bf15c15696a28e44b7d0a5eb770
James Blake <james.blake@RACKSPACE.COM>
2018-08-30 16:17:21 -0500

    Test lab error logs (#221)
    * compressed title, post step, wrap stages in parallel
    
    * remove quotes from env var in stage arg
    
    * stage names
    
    * bring functionality up a layer
    
    * fix dupe var
    
    * remove post
    
    * remove colon from steps
    
    * add slack prefix var
    
    * improve stage names
    
    * move environment up
    
    * post test
    
    * try post inner
    
    * add log copy / display to test 1, add conditional test runs, add per test confirmation
    
    * set param type
    
    * append param. in when expression
    
    * wire up all failure actions
    
    * add descriptions, try env.Var in stage name
    
    * try another
    
    * another
    
    * try single quote
    
    * .
    
    * shift logic around to accomodate selectable tests
    
    * fixups
    
    * fix gci call
    
    * add creds to copy logs
    
    * wire up test 1
    
    * take only first result, just in case
    
    * fix copy-item
    
    * make sure target dir exists
    
    * fix log verification
    
    * add log as artifcate on failure, link to correct log file, apply to all tests
    
    * move ip addresses to key store
    
    * set globals in script block
    
    * fix spelling errors, more descriptive slack logging
    
    * try different cred assignment for ip
    
    * fix env assignment
    
    * fix cred id assignment when running log copy script
    
    * fix log name in artifact statements
    
    * better logging
    
    * fix ip usages
    
    * download to view logs, remove temporary throw from main script
    
    * Add copy logs header
    
    * remove post
    
    * remove colon from steps
    
    * add slack prefix var
    
    * improve stage names
    
    * move environment up
    
    * post test
    
    * try post inner
    
    * add log copy / display to test 1, add conditional test runs, add per test confirmation
    
    * set param type
    
    * append param. in when expression
    
    * wire up all failure actions
    
    * add descriptions, try env.Var in stage name
    
    * try another
    
    * another
    
    * try single quote
    
    * .
    
    * shift logic around to accomodate selectable tests
    
    * fixups
    
    * fix gci call
    
    * add creds to copy logs
    
    * wire up test 1
    
    * take only first result, just in case
    
    * fix copy-item
    
    * make sure target dir exists
    
    * fix log verification
    
    * add log as artifcate on failure, link to correct log file, apply to all tests
    
    * move ip addresses to key store
    
    * set globals in script block
    
    * fix spelling errors, more descriptive slack logging
    
    * try different cred assignment for ip
    
    * fix env assignment
    
    * fix cred id assignment when running log copy script
    
    * fix log name in artifact statements
    
    * better logging
    
    * fix ip usages
    
    * download to view logs, remove temporary throw from main script
    
    * Add copy logs header
    
    * minor fixes for prepare output dir in copy-logs.ps1
    
    * fix up copy-logs header

---
+++ build/Copy-Logs.ps1
@@ -0,0 +1,105 @@
+<#
+
+    .SYNOPSIS
+        Copy EDU logs from a test lab.
+
+    .DESCRIPTION
+        Copy EDU logs from a test lab.
+
+    .PARAMETER BuildNumber
+        Used to find log files.
+        
+    .PARAMETER LabIpAddress
+        IP address the logs are located at.
+        
+    .PARAMETER Password
+        Password used to log onto the server specified by LabIpAddress.
+        
+    .PARAMETER Username
+        Username used to log onto the server specified by LabIpAddress.
+
+    .OUTPUTS
+        None
+
+#>
+
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $BuildNumber,
+
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $LabIpAddress,
+    
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $Password,
+    
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $Username
+)
+
+$ErrorActionPreference = "Stop"
+
+$remoteBaseFolder = "\\$LabIpAddress\c$\edu_ci"
+$remoteBuildFolder = "$remoteBaseFolder\$BuildNumber"
+
+function Map-PSDrive()
+{
+    $securePassword = ConvertTo-SecureString $Password -AsPlainText -Force
+    $credential = New-Object System.Management.Automation.PSCredential $Username, $securePassword
+
+    $labName = $LabIpAddress.Replace(".", "")
+
+    Write-Host "Connecting PSDrive to $remoteBaseFolder"
+
+    New-PSDrive -Name "EDU_CI_$labName" -PSProvider FileSystem -Root $remoteBaseFolder -Credential $credential 
+
+    Prepare-OutputDirectory
+}
+
+function Prepare-OutputDirectory()
+{
+     New-Item -ItemType Directory -Force -Path "$PSScriptRoot\$LabIpAddress"
+
+     Remove-Item "$PSScriptRoot\$LabIpAddress\*.log" -Force | Out-Null
+
+     Copy-Logs
+}
+
+function Copy-Logs()
+{
+    try
+    {
+        Get-ChildItem $remoteBuildFolder -Filter "*.log" -File | Select -First 1 | Copy-Item -Destination "$PSScriptRoot\$LabIpAddress\$LabIpAddress.log"
+    }
+    catch
+    {
+        Write-Host "Unable to copy log file $remoteBuildFolder\*.log to $PSScriptRoot\$LabIpAddress\$LabIpAddress.log"
+        throw
+    }
+
+    Comfirm-Logs
+}
+
+function Comfirm-Logs()
+{
+    if (Test-Path -Path "$PSScriptRoot\$LabIpAddress\$LabIpAddress.log" -PathType Leaf)
+    {
+        Write-Host "Verified logs were copied from $LabIpAddress"
+    }
+    else
+    {
+        Write-Host "Unable to find logs for $LabIpAddress"
+        throw
+    }
+}
+
+Map-PSDrive
